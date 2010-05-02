cbcb23f3ced2203b7483f7ec03fb997346852c17
Joshua Lock <incandescant@gmail.com>
2010-05-02 22:09:19 +0100

    Add App menu for switching between map and status view
    At some stage it'd be nice to have some better UI for this for now, however,
    this is perfectly usable.

--- app/assistants/mainView-assistant.js
+++ app/assistants/mainView-assistant.js
@@ -2,7 +2,22 @@ function MainViewAssistant() {
 }
 
 MainViewAssistant.prototype.setup = function() {
-    this.controller.setupWidget("mapScroller", { mode: "free" }, this.mapScrollerModel = {});
+    this.controller.setupWidget(Mojo.Menu.appMenu,
+                                this.attributes = {
+                                    omitDefaultItems: true
+                                },
+                                this.model = {
+                                    visible: true,
+                                   items: [
+                                        { label: "About Blunderground...",
+                                          command: "do-aboutBlunderground"},
+                                        { label: "Tube Status",
+                                          command: "do-tubeStatus"}
+                                    ]
+                                }
+                                );
+    this.controller.setupWidget("mapScroller", { mode: "free" },
+                                this.mapScrollerModel = {});
 };
 
 MainViewAssistant.prototype.activate = function(event) {
@@ -14,13 +29,24 @@ MainViewAssistant.prototype.deactivate = function(event) {
 MainViewAssistant.prototype.cleanup = function(event) {
 };
 
-/*
-// Show the current tube status
-MainViewAssistant.prototype.status = function(event) {
-  this.controller.stageController.swapScene(
-      {
-          transition: Mojo.Transition.crossFade,
-          name: "mapView"
+MainViewAssistant.prototype.handleCommand = function(event) {
+    var stageController = Mojo.Controller.getAppController().getActiveStageController();
+
+    if (event.type == Mojo.Event.command) {
+        switch(event.command) {
+        case "do-aboutBlunderground":
+            this.controller.showAlertDialog({
+                 onChoose: function(value) {},
+                 title: "Blunderground - an app for the Underground",
+                 message: "Copyright 2010, Joshua Lock",
+                 choices: [
+                     {label: "OK", value:""}
+                 ]
+            });
+            break;
+        case "do-tubeStatus":
+            stageController.swapScene("statusView");
+            break;
       }
-  );
-*/
+  }
+};
--- app/assistants/statusView-assistant.js
+++ app/assistants/statusView-assistant.js
@@ -1,31 +1,51 @@
 function StatusViewAssistant() {
-	/* this is the creator function for your scene assistant object. It will be passed all the 
-	   additional parameters (after the scene name) that were passed to pushScene. The reference
-	   to the scene controller (this.controller) has not be established yet, so any initialization
-	   that needs the scene controller should be done in the setup function below. */
 }
 
 StatusViewAssistant.prototype.setup = function() {
-	/* this function is for setup tasks that have to happen when the scene is first created */
-		
-	/* use Mojo.View.render to render view templates and add them to the scene, if needed */
-	
-	/* setup widgets here */
-	
-	/* add event handlers to listen to events from widgets */
+    this.controller.setupWidget(Mojo.Menu.appMenu,
+                                this.attributes = {
+                                    omitDefaultItems: true
+                                },
+                                this.model = {
+                                    visible: true,
+                                    items: [
+                                        { label: "About Blunderground...",
+                                          command: "do-aboutBlunderground"},
+                                        { label: "Tube Map",
+                                          command: "do-tubeMap"}
+                                    ]
+                                }
+                                );
 };
 
 StatusViewAssistant.prototype.activate = function(event) {
-	/* put in event handlers here that should only be in effect when this scene is active. For
-	   example, key handlers that are observing the document */
+
 };
 
 StatusViewAssistant.prototype.deactivate = function(event) {
-	/* remove any event handlers you added in activate and do any other cleanup that should happen before
-	   this scene is popped or another scene is pushed on top */
+
 };
 
 StatusViewAssistant.prototype.cleanup = function(event) {
-	/* this function should do any cleanup needed before the scene is destroyed as 
-	   a result of being popped off the scene stack */
+
+StatusViewAssistant.prototype.handleCommand = function(event) {
+    var stageController = Mojo.Controller.getAppController().getActiveStageController();
+
+    if (event.type == Mojo.Event.command) {
+        switch(event.command) {
+        case "do-aboutBlunderground":
+            this.controller.showAlertDialog({
+                 onChoose: function(value) {},
+                 title: "Blunderground - an app for the Underground",
+                 message: "Copyright 2010, Joshua Lock",
+                 choices: [
+                     {label: "OK", value:""}
+                 ]
+            });
+            break;
+        case "do-tubeMap":
+            stageController.swapScene("mainView");
+            break;
+      }
+  }
 };
