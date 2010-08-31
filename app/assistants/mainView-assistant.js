53837c3e4a65729bc091e5518633c6d01411283c
Neil Roberts <bpeeluk@yahoo.co.uk>
2010-08-31 23:56:07 +0100

    Add a 'jump to nearest' button
    This adds a button to the main view to jump to the nearest tube
    station. The current location is queried from the webOS location
    service. A separate Javascript file is added which contains a
    hard-coded list of coordinates for each station along with a pixel
    position inside the image. The scroller is scrolled to display the
    correct station. A banner div is added to the main view to report how
    far the station is away.

--- app/assistants/mainView-assistant.js
+++ app/assistants/mainView-assistant.js
@@ -1,6 +1,7 @@
 /*
  * Blunderground - an open source tube application for the Palm WebOS
  * Copyright (C) 2010 Joshua Lock. All rights reserved.
+ *               2010 Neil Roberts
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License version
@@ -17,12 +18,17 @@
  * 02110-1301, USA.
  *
  * Written by - Joshua Lock <joshual@joshual.me.uk>
+ *              Neil Roberts <bpeeluk@yahoo.co.uk>
  *
  */
 
 function MainViewAssistant(status) {
     this.status = status;
 }
+
+/* Approximate radius of the earth in kilometers (taken from
+ * Wikipedia) */
+MainViewAssistant.EARTH_RADIUS = 6371;
 
 MainViewAssistant.prototype.setup = function() {
     this.handleOrientation = this.handleOrientation.bindAsEventListener(this);
@@ -44,7 +50,8 @@ MainViewAssistant.prototype.setup = function() {
     this.commandMenuModel = {
         label: "Map Menu",
         items: [
-            {label: "Tube Status", command: "do-tubeStatus"}
+            {label: "Tube Status", command: "do-tubeStatus"},
+            {label: "Jump to Nearest", command: "do-jumpToNearest"}
         ]
     };
     this.controller.setupWidget(Mojo.Menu.commandMenu, undefined,
@@ -67,6 +74,92 @@ MainViewAssistant.prototype.cleanup = function(event) {
     this.controller.stopListening(document, 'orientationchange', this.handleOrientation);
 };
 
+MainViewAssistant.prototype.setBanner = function(msg) {
+    var banner = this.controller.get("mainViewBanner");
+    if (msg) {
+        banner.innerHTML = "<p>" + msg.escapeHTML() + "</p>";
+        banner.show();
+    } else {
+        banner.hide();
+    }
+};
+
+/* Based on http://mathforum.org/library/drmath/view/51722.html
+ * pos1 and pos2 are objects which contain a latitude and longitude
+ * property on degrees. The function returns a distance in
+ * kilometers. I think this isn't very accurate because it assumes the
+ * earth is a sphere */
+MainViewAssistant.prototype.calculateDistance = function(pos1, pos2) {
+    /* Convert to radians */
+    var lat1 = pos1.latitude * Math.PI / 180;
+    var lon1 = pos1.longitude * Math.PI / 180;
+    var lat2 = pos2.latitude * Math.PI / 180;
+    var lon2 = pos2.longitude * Math.PI / 180;
+
+    var aob = Math.acos(Math.cos(lat1) * Math.cos(lat2) *
+                        Math.cos(lon2 - lon1) +
+                        Math.sin(lat1) * Math.sin(lat2));
+
+    return MainViewAssistant.EARTH_RADIUS * aob;
+};
+
+MainViewAssistant.prototype.prettyDistance = function(distance) {
+    if (distance >= 1)
+        return Mojo.Format.formatNumber(distance, {
+                fractionDigits: 2 }) + "km";
+    else if (distance >= 0.001)
+        return Mojo.Format.formatNumber(distance * 1000, {
+                fractionDigits: 2 }) + "m";
+    else
+        return Mojo.Format.formatNumber(distance * 100000, {
+                fractionDigits: 2 }) + "cm";
+}
+
+MainViewAssistant.prototype.doJumpToNearest = function(response) {
+    var bestStation;
+    var bestDistance;
+    var stationNum;
+
+    /* Find the nearest station. There's probably much better ways to
+     * do this */
+    for (stationNum = 0; stationNum < STATION_DATA.length; stationNum++) {
+        var station = STATION_DATA[stationNum];
+        var distance = this.calculateDistance(response, station);
+        if (bestStation == undefined || bestDistance > distance) {
+            bestStation = station;
+            bestDistance = distance;
+        }
+    }
+
+    this.setBanner("Your nearest station is " + bestStation.name +
+                   " which is " + this.prettyDistance(bestDistance) + " away");
+
+    var scroller = $("mapScroller");
+    scroller.mojo.scrollTo(scroller.offsetWidth / 2.0 - bestStation.imagex,
+                           /* FIXME - using the offsetHeight doesn't
+                            * work well here */
+                           100 - bestStation.imagey,
+                           true);
+};
+
+MainViewAssistant.prototype.notifyNoLocation = function(response) {
+    this.setBanner("Unable to get your current location");
+};
+
+MainViewAssistant.prototype.jumpToNearest = function() {
+    this.setBanner("Getting location...");
+
+    this.controller.serviceRequest('palm://com.palm.location', {
+            method: 'getCurrentPosition',
+            /* Set the maximum age parameter to let the service know
+             * that we can accept a cached value that is less than 5
+             * seconds old */
+            parameters: { maximumAge: 5 },
+            onSuccess: this.doJumpToNearest.bind(this),
+            onFailure: this.notifyNoLocation.bind(this)
+        });
+};
+
 MainViewAssistant.prototype.handleCommand = function(event) {
     var stageController = Mojo.Controller.getAppController().getActiveStageController();
 
@@ -84,6 +177,9 @@ MainViewAssistant.prototype.handleCommand = function(event) {
             break;
         case "do-tubeStatus":
             stageController.swapScene("statusView", this.status);
+            break;
+        case "do-jumpToNearest":
+            this.jumpToNearest();
             break;
       }
   }
